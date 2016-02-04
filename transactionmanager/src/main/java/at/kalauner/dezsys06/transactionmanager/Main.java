b104c8eda5bacb9a6b3005a8911da543a0eeb1fa
Paul Kalauner <pkalauner@student.tgm.ac.at>
2016-02-04 08:46:02 +0100

    Initial commit

---
+++ transactionmanager/src/main/java/at/kalauner/dezsys06/transactionmanager/Main.java
@@ -0,0 +1,22 @@
+package at.kalauner.dezsys06.transactionmanager;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Main-Class
+ *
+ * @author Paul Kalauner 5BHIT
+ * @version 1.0
+ */
+public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+    /**
+     * Main-Method
+     * @param args CLI-arguments
+     */
+    public static void main(String[] args) {
+        LOGGER.info("Starting Transactionmanager...");
+       // START
+    }
+}
